54a09b103e4711a3f47376624ca9c00cdbf09ffc
Vince <paystaxes@protonmail.com>
2020-09-23 17:00:32 -0400

    UI First Pass
    + Connection Panel
    + Username Panel
    + Gameplay Panel
    + Chat Panel
    + GameBoard Panel
    + GameBoard Background Panel
    + Sprites for empty slot, blue filled slot, red filled slot

---
+++ Assets/Vince/Scripts/ButtonGamePiece.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public struct GridPOS
+{
+    public int X;
+    public int Y;
+    public GridPOS(int X, int Y)
+    {
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public override string ToString()
+    {
+        return $"Grid Position: ({X}, {Y})";
+    }
+}
+public class ButtonGamePiece : MonoBehaviour
+{
+    public GridPOS pos;
+    public void Init(GridPOS pos, UnityAction callback)
+    {
+        this.pos = pos;
+        Button bttn = GetComponent<Button>();
+
+        //bttn.onClick.AddListener(new UnityEngine.Events.UnityAction(ButtonClicked));
+        //bttn.onClick.AddListener(() => ButtonClicked());
+        bttn.onClick.AddListener(callback);
+
+    }
+    public void ButtonClicked()
+    {
+        print("Ooh that tickles...");
+    }
+}
---
+++ Assets/Vince/Scripts/ControllerGameplay.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Player
+{
+    Nobody,
+    PlayerRed,
+    PlayerBlue
+}
+public class ControllerGameplay : MonoBehaviour
+{
+    private int columns = 7;
+    private int rows = 6;
+
+    public ButtonGamePiece bttnPrefab;
+
+    private Player whoseTurn = Player.PlayerRed;
+    private Player[,] boardData; // all of the data of who owns what
+    private ButtonGamePiece[,] boardUI; // all of the buttons
+
+    public Transform panelGameBoard; // grid of buttons
+
+    void Start()
+    {
+        BuildBoardUI();
+    }
+
+    private void BuildBoardUI()
+    {
+        boardUI = new ButtonGamePiece[columns, rows]; // instantiating array for buttons
+
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                ButtonGamePiece bttn = Instantiate(bttnPrefab, panelGameBoard);
+                bttn.Init(new GridPOS(x, y), () => { ButtonClicked(bttn); }); // anonymous functions have access to all variables in their scope when declared
+                boardUI[x, y] = bttn;
+            }
+        }
+    }
+
+    void ButtonClicked(ButtonGamePiece bttn)
+    {
+        //print($"X:{bttn.pos.X} Y:{bttn.pos.Y} was clicked");
+        print($"Button was clicked. {bttn.pos} ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
